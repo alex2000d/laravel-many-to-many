c9fff5678606e6d6762bbce8db8a0127955f3ad5
alex2000d <demarcoalessandro777@gmail.com>
2024-10-10 18:03:06 +0200

    [UPD] populated the technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\technology;
+use Illuminate\Support\Str;
 class TechnologySeeder extends Seeder
 {
     /**
@@ -14,6 +15,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Javascript', 'PHP','Laravel', 'Vuejs', 'Reactjs', 'Nodejs'];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new technology(); 
+            $new_technology->name = $technology; 
+            $new_technology->slug = Str::slug($technology, '-'); 
+            $new_technology->save(); 
+        }
     }
 }
